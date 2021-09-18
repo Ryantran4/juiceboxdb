f1a726c4af19bdc46586fa8493982ed92e6743b4
Ryan Tran <ryantran9@yahoo.com>
2021-09-18 11:40:32 -0700

    part2 wrapup

--- api/users.js
+++ api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { JsonWebTokenError } = require('jsonwebtoken');
 const usersRouter = express.Router();
 
-const { getAllUsers, getUserByUsername } = require('../db');
+const { getAllUsers, getUserByUsername, createUser } = require('../db');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 
@@ -59,8 +59,41 @@ usersRouter.post('/login', async (req, res, next) => {
     }
   });
 
+  usersRouter.post('/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
   
-// }})
+    try {
+      const _user = await getUserByUsername(username);
+  
+      if (_user) {
+        next({
+          name: 'UserExistsError',
+          message: 'A user by that username already exists'
+        });
+      }
+  
+      const user = await createUser({
+        username,
+        password,
+        name,
+        location,
+      });
+  
+      const token = jwt.sign({ 
+        id: user.id, 
+        username
+      }, process.env.JWT_SECRET, {
+        expiresIn: '1w'
+      });
+  
+      res.send({ 
+        message: "thank you for signing up",
+        token 
+      });
+    } catch ({ name, message }) {
+      next({ name, message })
+    } 
+  });
 
 module.exports = usersRouter;
 
